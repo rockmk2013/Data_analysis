0fbb57781f999862738aa8638a68e3e7cdb4bc5f
郭泰竹 <102306037@nccu.edu.tw>
2017-07-25 15:20:28 +0800

    FA library revised

--- tabFA.R
+++ tabFA.R
@@ -1,13 +1,5 @@
 library(shiny)
-library(ggplot2)
-library(dplyr)
-library(psych)
-library(nFactors)
-library(readr)
-library(gWidgets)
-library(scales)
-library(xtable)
-library(corrplot)
+
 
 fa_return <- reactive({
   # Read data
